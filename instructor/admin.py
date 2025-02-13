0740fea4cf85a11db6ca7d2facbc9aca47056a08
Ananya Krishhna R <ananyakrishnar98@gmail.com>
2025-02-13 07:30:21 +0530

    changes in course model

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from instructor.models import User,Catagory
+from instructor.models import User,Catagory,Course
 admin.site.register(User)
 admin.site.register(Catagory)
+admin.site.register(Course)
+
